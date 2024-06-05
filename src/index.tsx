3c7e2f50ef870afa79b1a8ebb88a11980e9487ab
ThaoVy <thaovyluuthanh@gmail.com>
2024-06-05 22:21:06 +0700

    send email

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,25 @@ import App from "./App";
 import { store } from "./redux/store/store";
 
 import "./index.scss";
+import emailjs from "@emailjs/browser";
+
+emailjs.init({
+  publicKey: "g3ukIcsb_jLDAbXD1",
+  // Do not allow headless browsers
+  blockHeadless: true,
+  blockList: {
+    // Block the suspended emails
+    list: ["foo@emailjs.com", "bar@emailjs.com"],
+    // The variable contains the email address
+    watchVariable: "userEmail",
+  },
+  limitRate: {
+    // Set the limit rate for the application
+    id: "app",
+    // Allow 1 request per 10s
+    throttle: 10000,
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
